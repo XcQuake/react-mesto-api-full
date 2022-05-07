0dd3239c934e339dfed561b52ba2b66f8e86059c
XcQuid <creed.35@yandex.ru>
2022-05-07 22:30:54 +0300

    fix: add populate function in like/dislike controllers

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -49,6 +49,7 @@ module.exports.deleteCard = (req, res, next) => {
 module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .orFail(() => { throw new NotFoundError('Передан несуществующий _id карточки.'); })
+    .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -62,6 +63,7 @@ module.exports.likeCard = (req, res, next) => {
 module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .orFail(() => { throw new NotFoundError('Передан несуществующий _id карточки.'); })
+    .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'CastError') {
